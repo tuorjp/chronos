911a0edcc0dfde4605f60314301f5c11c9e9b614
JP <jplmarotinho22@gmail.com>
2025-06-25 20:58:40 -0300

    Container Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
-import { TimerIcon } from 'lucide-react'
-import { Heading } from './components/Heading/Heading'
 import './styles/global.css'
 import './styles/theme.css'
+import { Container } from './components/Container/Container';
+import { Heading } from './components/Heading/Heading';
 
 export default function App() {
 
   return (
     <>
-      <Heading>
-        TITULO
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ipsa voluptate, suscipit quia nesciunt fugiat atque illum illo nobis quidem, ullam excepturi enim at error totam libero dignissimos, eius architecto?
-      </p>
+      <Container>
+        <Heading>Logo</Heading>
+      </Container>
+      <Container>
+        <Heading>Menu</Heading>
+      </Container>
     </>
   )
 }
