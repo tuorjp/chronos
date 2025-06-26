50c844eeb8aa1cfbbe2b699ffb8a053de4a11d62
JP <jplmarotinho22@gmail.com>
2025-06-25 21:18:43 -0300

    Componente Logo

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import './styles/global.css'
 import './styles/theme.css'
 import { Container } from './components/Container';
 import { Heading } from './components/Heading';
+import { Logo } from './components/Logo';
 
 export default function App() {
 
   return (
     <>
       <Container>
-        <Heading>Logo</Heading>
+        <Logo />
       </Container>
       <Container>
         <Heading>Menu</Heading>
---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,13 @@
+import styles from './styles.module.css'
+import { TimerIcon } from 'lucide-react';
+
+export function Logo() {
+  return (
+    <div className={styles.logo}>
+      <a href="" className={styles.logoLink}>
+        <TimerIcon />
+        <span>CHRONOS</span>
+      </a>
+    </div>
+  )
+}
